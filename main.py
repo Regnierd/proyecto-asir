b51d6aca93e173c8ea55610f9ea646d0034dcb5a
javier lima perez <javivi_lp_96@hotmail.es>
2020-03-24 21:06:12 +0000

    u

--- main.py
+++ main.py
@@ -2,14 +2,29 @@ from flask import Flask
 from flask import render_template
 from flask import redirect
 from flask import request
+from flask import flash
+from flask import session
 from program import users, loggins
-
+#from bcrypt import bcrypt
 
 app = Flask(__name__)
 
+#codigo para encriptamiento
+#code = bcrypt.gensalt()
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if 'name' in session:
+        return render_template('principal.html')
+    else:
+        return render_template('index.html')
+
+@app.route('/inicio')
+def inicio():
+    if 'name' in session:
+        return render_template('principal.html')
+    else:
+        return render_template('index.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def create_user():
@@ -17,24 +32,32 @@ def create_user():
         name = request.form["name"]
         email = request.form["email"]
         password = request.form["password"]
+#        password_encode = password_encode.encode("utf-8")
+#        password_encriptada = bcrypt.hashwp(password_encode, code)
         user = users(name, email, password)
         user.add_user()
-        return redirect('/')
+
+        #Hago un registro de la sesión.
+#        session["name"] = name
+#        session["email"] = email
+        return redirect('/index')
     else:
         return render_template('register.html')
 
 @app.route('/index', methods=['GET', 'POST'])
 def connected():
-    if request.method == 'POST':
-        try:
-            email = request.form["email"]
-            password = request.form["password"]
-            log = loggins(email, password)
-            log.login()
+    if request.method == 'GET':
+        if 'name' in session:
             return render_template('principal.html')
+        else:
+            return render_template('index.html')
+    else:
+        email = request.form["email"]
+        password = request.form["password"]
+        log = loggins(email, password)
+        log.login()
+        return render_template('principal.html')
 
-        except IndexError:
-            return render_template('index.html', message='No estas registrado')
 
 if __name__ == "__main__":
     app.run(debug = True)
